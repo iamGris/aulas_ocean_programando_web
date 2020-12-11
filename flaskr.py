23e1e9af9cd55a2b9b16c53400d4c1069cdbcf45
iamGris <crissk53@hotmail.com>
2020-12-11 19:05:56 +0000

    Aulas pagina web

--- flaskr.py
+++ flaskr.py
@@ -4,13 +4,27 @@ from flask import Flask, render_template, request, g, redirect, url_for, \
 
 # Configurações
 
-
 DATABASE = './tmp/flaskr.db'
 USERNAME = 'admin'
 PASSWORD = '123456'
 
-# Criando o APP
+# Criando o App
 app = Flask(__name__)
 
 def conectar_bd():
     return sqlite3.connect(DATABASE)
+
+@app.before_request
+def pre_requisicao():
+    g.bd = conectar_bd()
+
+@app.teardown_request
+def pos_requisicao(exception):
+    g.bd.close()
+
+@app.route('/')
+def exibir_entradas():
+    sql = "SELECT titulo, texto FROM entradas ORDER BY id DESC"
+    cursor = g.bd.execute(sql)
+    entradas = [dict(titulo=titulo, texto=texto) for titulo, texto in cursor.fetchall()]
+    return render_template('exibir_entradas.html')
